380108c046fab9318770150a8179ef596d7c84d5
hanishbansal231 <108611960+hanishbansal231@users.noreply.github.com>
2023-03-16 17:52:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onscroll = function () { scrollProgreesBar() };
+
+function scrollProgreesBar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
+
